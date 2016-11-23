ba611c4c45ec2db57ae68c1d84cc8b222492386f
Suvan <suvanramani@gmail.com>
2016-11-23 12:33:36 -0800

    Initial Commit

---
+++ EveryDoAgain/DetailViewController.h
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.h
+//  EveryDoAgain
+//
+//  Created by Suvan Ramani on 2016-11-23.
+//  Copyright © 2016 suvanr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "EveryDoAgain+CoreDataModel.h"
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) Event *detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ EveryDoAgain/MasterViewController.h
@@ -0,0 +1,24 @@
+//
+//  MasterViewController.h
+//  EveryDoAgain
+//
+//  Created by Suvan Ramani on 2016-11-23.
+//  Copyright © 2016 suvanr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+#import "EveryDoAgain+CoreDataModel.h"
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController<Event *> *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
